bbe3184137faee14ef95038faa3f2016feee490b
iumehara <iumehara10@gmail.com>
2018-08-19 18:22:57 +0900

    adds tests for p1wins, p2wins, tie, and invalid

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,17 +1,112 @@
 describe('play', () => {
-  it('rock vs scissors', () => {
-    const player1WinsSpy = jasmine.createSpy()
-    const observer = {
-      player1Wins: player1WinsSpy
-    }
-    new Requests().play('rock', 'scissors', observer)
+  describe('player 1 wins', () => {
+    it('rock vs scissors', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('rock', 'scissors', observerSpy)
 
-    expect(player1WinsSpy).toHaveBeenCalled()
+      expect(observerSpy.p1Wins).toHaveBeenCalled()
+    })
+
+    it('paper vs rock', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('paper', 'rock', observerSpy)
+
+      expect(observerSpy.p1Wins).toHaveBeenCalled()
+    })
+
+    it('scissors vs paper', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('scissors', 'paper', observerSpy)
+
+      expect(observerSpy.p1Wins).toHaveBeenCalled()
+    })
+  })
+
+  describe('player 2 wins', () => {
+    it('rock vs paper', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('rock', 'paper', observerSpy)
+
+      expect(observerSpy.p2Wins).toHaveBeenCalled()
+    })
+
+    it('paper vs scissors', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('paper', 'scissors', observerSpy)
+
+      expect(observerSpy.p2Wins).toHaveBeenCalled()
+    })
+
+    it('scissors vs rock', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('scissors', 'rock', observerSpy)
+
+      expect(observerSpy.p2Wins).toHaveBeenCalled()
+    })
+  })
+
+  describe('tie', () => {
+    it('rock vs rock', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('rock', 'rock', observerSpy)
+
+      expect(observerSpy.tie).toHaveBeenCalled()
+    })
+    it('paper vs paper', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('paper', 'paper', observerSpy)
+
+      expect(observerSpy.tie).toHaveBeenCalled()
+    })
+    it('scissors vs scissors', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('scissors', 'scissors', observerSpy)
+
+      expect(observerSpy.tie).toHaveBeenCalled()
+    })
+  })
+
+  describe('invalid', () => {
+    it('sailboat vs rock', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('sailboat', 'rock', observerSpy)
+
+      expect(observerSpy.invalid).toHaveBeenCalled()
+    })
+
+    it('rock vs sailboat', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('rock', 'sailboat', observerSpy)
+
+      expect(observerSpy.invalid).toHaveBeenCalled()
+    })
+
+    it('sailboat vs sailboat', () => {
+      const observerSpy = jasmine.createSpyObj('player1Wins', ['p1Wins', 'p2Wins', 'tie', 'invalid'])
+      new Requests().play('sailboat', 'sailboat', observerSpy)
+
+      expect(observerSpy.invalid).toHaveBeenCalled()
+    })
   })
 })
 
 function Requests() {
   this.play = (p1Hand, p2Hand, observer) => {
-    observer.player1Wins()
+    if (
+      !['paper', 'rock', 'scissors'].includes(p1Hand) ||
+      !['paper', 'rock', 'scissors'].includes(p2Hand)
+    ) {
+      observer.invalid()
+    } else if (p1Hand === p2Hand) {
+      observer.tie()
+    } else if (
+      p1Hand === 'rock' && p2Hand === 'paper' ||
+      p1Hand === 'paper' && p2Hand === 'scissors' ||
+      p1Hand === 'scissors' && p2Hand === 'rock'
+    ) {
+      observer.p2Wins()
+    } else {
+      observer.p1Wins()
+    }
   }
 }
