1731d75537dc994670a04b63468f5efe9dbbecb7
iumehara <iumehara10@gmail.com>
2018-08-19 17:27:45 +0900

    Adds observer to play function

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,13 +1,17 @@
 describe('play', () => {
-    it('rock vs scissors', () => {
-        const result = new Requests().play('rock', 'scissors')
+  it('rock vs scissors', () => {
+    const player1WinsSpy = jasmine.createSpy()
+    const observer = {
+      player1Wins: player1WinsSpy
+    }
+    new Requests().play('rock', 'scissors', observer)
 
-        expect(result).toBe('Player 1 Wins')
-    })
+    expect(player1WinsSpy).toHaveBeenCalled()
+  })
 })
 
 function Requests() {
-    this.play = () => {
-        return 'Player 1 Wins'
-    }
+  this.play = (p1Hand, p2Hand, observer) => {
+    observer.player1Wins()
+  }
 }
