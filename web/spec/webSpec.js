dfc55230ef70178aad65133eecb445977ba1fc97
iumehara <iumehara10@gmail.com>
2018-08-23 23:15:33 +0900

    completes all tests for playform

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,28 +1,97 @@
 const ReactDOM = require('react-dom')
 const React = require('react')
+const ReactTestUtils = require('react-dom/test-utils')
 const PlayForm = require('../src/PlayForm')
 
 describe('play form', function () {
+  let domFixture
+  beforeEach(() => {
+    domFixture = document.createElement('div')
+    document.querySelector('body').appendChild(domFixture)
+  })
+  afterEach(() => {
+    domFixture.remove()
+  })
+
   describe('when request tells web UI that input is invalid', function () {
     it('displays invalid input', function () {
-      const domFixture = document.createElement('div')
-      document.querySelector('body').appendChild(domFixture)
-      const requests = {
-        play: (p1, p2, uiObserver) => uiObserver.invalid()
-      }
-
-
+      const requests = {play: (p1, p2, uiObserver) => uiObserver.invalid()}
       ReactDOM.render(
         <PlayForm requests = {requests}/>,
         domFixture
       )
 
-      const expectedText = 'invalid input'
+      const expectedText = 'Invalid Input'
       expect(domFixture.innerText).not.toContain(expectedText)
-
       document.querySelector('button').click()
-
       expect(domFixture.innerText).toContain(expectedText)
     })
   })
+
+  describe('when request tells web UI that result is tied', function () {
+    it('displays TIE', function () {
+      const requests = {play: (p1, p2, uiObserver) => uiObserver.tie()}
+      ReactDOM.render(
+        <PlayForm requests = {requests}/>,
+        domFixture
+      )
+
+      const expectedText = 'Players Tie'
+      expect(domFixture.innerText).not.toContain(expectedText)
+      document.querySelector('button').click()
+      expect(domFixture.innerText).toContain(expectedText)
+    })
+  })
+
+  describe('when request tells web UI that player one won', function () {
+    it('displays Player 1 Wins!', function () {
+      const requests = {play: (p1, p2, uiObserver) => uiObserver.p1Wins()}
+      ReactDOM.render(
+        <PlayForm requests = {requests}/>,
+        domFixture
+      )
+
+      const expectedText = 'Player 1 Wins!'
+      expect(domFixture.innerText).not.toContain(expectedText)
+      document.querySelector('button').click()
+      expect(domFixture.innerText).toContain(expectedText)
+    })
+  })
+
+  describe('when request tells web UI that player two won', function () {
+    it('displays Player 2 Wins!', function () {
+      const requests = {play: (p1, p2, uiObserver) => uiObserver.p2Wins()}
+      ReactDOM.render(
+        <PlayForm requests = {requests}/>,
+        domFixture
+      )
+
+      const expectedText = 'Player 2 Wins!'
+      expect(domFixture.innerText).not.toContain(expectedText)
+      document.querySelector('button').click()
+      expect(domFixture.innerText).toContain(expectedText)
+    })
+  })
+
+  it('sends the users input to the play request', () => {
+    const playSpy = jasmine.createSpy('playSpy')
+    const requests = {play: playSpy}
+    ReactDOM.render(
+      <PlayForm requests = {requests}/>,
+      domFixture
+    )
+
+    fillInInputWith('p1Throw', 'rock')
+    fillInInputWith('p2Throw', 'paper')
+    document.querySelector('button').click()
+
+    expect(playSpy).toHaveBeenCalledWith('rock', 'paper', jasmine.any(Object))
+  })
+
+  function fillInInputWith(inputName, inputValue) {
+    let input = document.querySelector(`[name='${inputName}']`)
+    input.value = inputValue
+    ReactTestUtils.Simulate.change(input)
+  }
 })
+
--- web/src/PlayForm.js
+++ web/src/PlayForm.js
@@ -6,15 +6,37 @@ class PlayForm extends React.Component {
     this.state = {}
   }
 
-  playButtonWasClicked() {
-    this.setState({result: 'invalid input'})
+  playButtonClicked() {
+    this.props.requests.play(this.state.p1Throw, this.state.p2Throw, this)
+  }
+
+  invalid(){
+    this.setState({result: 'Invalid Input'})
+  }
+
+  p1Wins(){
+    this.setState({result: 'Player 1 Wins!'})
+  }
+
+  p2Wins(){
+    this.setState({result: 'Player 2 Wins!'})
+  }
+
+  tie(){
+    this.setState({result: 'Players Tie'})
+  }
+
+  inputChanged(e){
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
     return (
       <div>
         <div>{this.state.result}</div>
-        <button onClick={this.playButtonWasClicked.bind(this)}>
+        <input name="p1Throw" onChange={this.inputChanged.bind(this)}/>
+        <input name="p2Throw" onChange={this.inputChanged.bind(this)}/>
+        <button onClick={this.playButtonClicked.bind(this)}>
           play
         </button>
       </div>
