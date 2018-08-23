a5dd292659190b105b6c959361b53e95e159500f
iumehara <iumehara10@gmail.com>
2018-08-23 22:01:47 +0900

    moves Requests to src directory

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,3 +1,5 @@
+const Requests = require('../src/Requests')
+
 describe('play', () => {
   describe('player 1 wins', () => {
     it('rock vs scissors', () => {
@@ -89,36 +91,3 @@ describe('play', () => {
     })
   })
 })
-
-function Requests() {
-  this.play = (p1Hand, p2Hand, observer) => new PlayRequest(p1Hand, p2Hand, observer).process()
-}
-
-function PlayRequest(p1Hand, p2Hand, observer) {
-  this.process = () => {
-    if (invalidHand()) {
-      observer.invalid()
-    } else if (isTie()) {
-      observer.tie()
-    } else if (player2Wins()) {
-      observer.p2Wins()
-    } else {
-      observer.p1Wins()
-    }
-  }
-
-  function invalidHand() {
-    return !['paper', 'rock', 'scissors'].includes(p1Hand) ||
-      !['paper', 'rock', 'scissors'].includes(p2Hand)
-  }
-
-  function isTie() {
-    return p1Hand === p2Hand
-  }
-
-  function player2Wins() {
-    return p1Hand === 'rock' && p2Hand === 'paper' ||
-      p1Hand === 'paper' && p2Hand === 'scissors' ||
-      p1Hand === 'scissors' && p2Hand === 'rock'
-  }
-}
